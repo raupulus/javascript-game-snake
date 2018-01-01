359736edf8423e792f900c877c245306ffc3dc3d
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-01 21:26:10 +0100

    Planteadas funciones

--- js/player.js
+++ js/player.js
@@ -3,3 +3,39 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        //this.score = new Score(0);
+        //this.level = 0
+    }
+
+    get nombre() {
+        return this.name
+    }
+
+    set nombre() {
+        this.name = name;
+    }
+
+    get puntuacion() {
+
+    }
+
+    set puntuacion() {
+
+    }
+
+    get nivel() {
+
+    }
+
+    aumentarPuntuacion() {
+
+    }
+
+    subirNivel() {
+
+    }
+}
