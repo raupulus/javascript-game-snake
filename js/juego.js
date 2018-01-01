559bd39e03279e63ae57a2f1c50ba091278aa9c1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-02 00:57:38 +0100

    Función para mostrar la información de usuario planteada

--- js/juego.js
+++ js/juego.js
@@ -3,3 +3,15 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+/**
+ * Pinta toda la información del usuario en su caja correspondiente
+ * Dicha caja será un <div> con el id="cajajugador"
+ */
+function info_user() {
+    jugador = new Player('Manolo');
+    cajajuego.innerHTML += 'Datos del Jugador';
+    cajajuego.innerHTML += '<br />Nombre del jugador → ' + jugador.nombre;
+    cajajuego.innerHTML += '<br />Puntuación → ' + jugador.puntuacion;
+    cajajuego.innerHTML += '<br />Nivel → ' + jugador.nivel;
+}
