e246952071c75714f4daf970298c5dfbfcfae816
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-01 23:49:53 +0100

    Planteada clase Manzana

--- js/manzana.js
+++ js/manzana.js
@@ -3,3 +3,15 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+/**
+ * Clase para cada manzana del mapa.
+ */
+class Manzana {
+    /**
+     * Constructor que genera una nueva instancia de manzana
+     */
+    constructor() {
+        // this.posicion = Random según dimensiones del mapa
+    }
+}
