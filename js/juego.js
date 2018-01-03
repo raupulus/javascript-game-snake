dfdd5b0da9c14edaaa87465b442a00f2b0f4dbd7
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-04 00:06:52 +0100

    Intervalo para actualizar información del jugador cada segundo

--- js/juego.js
+++ js/juego.js
@@ -3,3 +3,6 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
 */
+
+// Actualiza la información de usuario cada segundo
+setInterval(info_user, 1000);
