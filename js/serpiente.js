be3efe347ed04dfd2091a786e0126f4469daf498
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-04 16:56:00 +0100

    GET y SET para dirección

--- js/serpiente.js
+++ js/serpiente.js
@@ -12,11 +12,12 @@ class Serpiente {
      * Constructor que define la serpiente y el lugar que ocupa.
      */
     constructor() {
-        this.cabeza   = 0;        // Posición de la cabeza.
-        this.cola     = 1;        // Posición de la cola.
-        this.altura   = 10;       // La altura en píxeles de la serpiente
-        this.anchura  = 10;       // El ancho en píxeles de la serpiente
+        this.cabeza    = 0;        // Posición de la cabeza.
+        this.cola      = 1;        // Posición de la cola.
+        this.altura    = 10;       // La altura en píxeles de la serpiente
+        this.anchura   = 10;       // El ancho en píxeles de la serpiente
         this.colour    = 'green';  // Color de la serpiente
+        this.direction = 'R'       // Dirección en la que mira la serpiente
     }
 
     crecer() {
@@ -37,5 +38,13 @@ class Serpiente {
 
     get color() {
         return this.colour;
+    }
+
+    set direccion(dir) {
+        this.direction = dir;
+    }
+
+    get direccion() {
+        return this.direction;
     }
 }
