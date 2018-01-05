fe00f97e18c63705b212241f8adf7d3a898d4bd0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-05 21:11:32 +0100

    Agregada documentación a los métodos.

--- js/serpiente.js
+++ js/serpiente.js
@@ -20,30 +20,43 @@ class Serpiente {
         this.direction = 'R';       // Dirección en la que mira la serpiente
     }
 
-    crecer() {
-        this.cola += 1;
-    }
-
-    get longitud() {
-        return this.cola + 1;
-    }
-
+    /**
+     * Grosor de ancho para la serpiente.
+     * @return {Integer} Devuelve el número de píxeles que ocupa el ancho.
+     */
     get ancho() {
         return this.anchura;
     }
 
+    /**
+     * Altura que tiene la serpiente.
+     * @return {Integer} Devuelve el número de píxeles que ocupa el alto.
+     */
     get alto() {
         return this.altura;
     }
 
+    /**
+     * Color que tiene la serpiente.
+     * @return {String} Devuelve el color con el que será dibujada la serpiente.
+     */
     get color() {
         return this.colour;
     }
 
+    /**
+     * Establece una nueva dirección hacia la cual está mirando la serpiente.
+     * @param  {Char} dir Recibe el carácter inicial de Up, Right, Left, Down.
+     */
     set direccion(dir) {
         this.direction = dir;
     }
 
+    /**
+     * Devuelve la dirección sobre la que está mirando la serpiente.
+     * @return {Char} Devuelve el caracter sobre la dirección la cual
+     *                está mirando la serpiente.
+     */
     get direccion() {
         return this.direction;
     }
